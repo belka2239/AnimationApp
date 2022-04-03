88b2087b45d455da056ba91435d1c604b297ed2c
Gurina Elena <belka2239@gmail.com>
2022-04-03 13:46:11 +0300

    Work with ViewController

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -5,15 +5,34 @@
 //  Created by VG on 30.03.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IB Outlets
+    @IBOutlet var springAnimationView: SpringView!
+    @IBOutlet var animationLabel: UILabel!
+    
+    // MARK: - Private properties
+    private var animation = Animation.getRandomAnimation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animationLabel.text = animation.fullDescription
     }
 
-
+    // MARK: - IB Action
+    @IBAction func springAnimationButtonPressed(_ sender: SpringButton) {
+        animationLabel.text = animation.fullDescription
+        springAnimationView.animation = animation.name
+        springAnimationView.force = CGFloat(animation.force)
+        springAnimationView.duration = CGFloat(animation.duration)
+        springAnimationView.delay = CGFloat(animation.delay)
+        springAnimationView.curve = animation.curve
+        springAnimationView.animate()
+        
+        animation = Animation.getRandomAnimation()
+        sender.setTitle("Run \(animation.name)", for: .normal)
+    }
 }
 
